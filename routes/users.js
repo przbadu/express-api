e23dc30782537dea8503197dc4c3941876e0ad07
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2017-07-30 17:14:18 +0545

    Add signup server with validation

--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,7 @@
 import express from 'express';
-import validator from 'validator';
-import { isEmpty } from 'lodash';
+import validateInput from '../shared/validations/signup';
 
 let router = express.Router();
-
-function validateInput(data) {
-  let errors = {};
-
-  if (validator.isEmpty(data.username)) {
-    errors.username = "is required"
-  }
-
-  if (validator.isEmpty(data.email)) {
-    errors.email = 'is required';
-  }
-
-  if (!validator.isEmail(data.email)) {
-    errors.email = 'invalid email!';
-  }
-
-  if (validator.isEmpty(data.password)) {
-    errors.password = 'is required';
-  }
-
-  if (validator.isEmpty(data.passwordConfirmation)) {
-    errors.passwordConfirmation = 'is required';
-  }
-
-  if (!validator.equals(data.password, data.passwordConfirmation)) {
-    errors.passwordConfirmation = "Passwords must match";
-  }
-
-  if (validator.isEmpty(data.timezone)) {
-    errors.timezone = 'is required';
-  }
-
-  return {
-    errors,
-    isValid: isEmpty(errors)
-  }
-}
-
 
 router.post('/', (req, res) => {
   const { errors, isValid } = validateInput(req.body);
---
+++ shared/validations/signup.js
@@ -0,0 +1,39 @@
+import validator from 'validator';
+import { isEmpty } from 'lodash';
+
+export function validateInput(data) {
+  let errors = {};
+
+  if (validator.isEmpty(data.username)) {
+    errors.username = "is required"
+  }
+
+  if (validator.isEmpty(data.email)) {
+    errors.email = 'is required';
+  }
+
+  if (!validator.isEmail(data.email)) {
+    errors.email = 'invalid email!';
+  }
+
+  if (validator.isEmpty(data.password)) {
+    errors.password = 'is required';
+  }
+
+  if (validator.isEmpty(data.passwordConfirmation)) {
+    errors.passwordConfirmation = 'is required';
+  }
+
+  if (!validator.equals(data.password, data.passwordConfirmation)) {
+    errors.passwordConfirmation = "Passwords must match";
+  }
+
+  if (validator.isEmpty(data.timezone)) {
+    errors.timezone = 'is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  }
+}
