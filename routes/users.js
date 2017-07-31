0b558c4228e66aded4a820ec6dee802a040aa397
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2017-07-31 08:37:30 +0545

    return success response

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import validateInput from '../shared/validations/signup';
+import { validateInput } from '../shared/validations/signup';
 
 let router = express.Router();
 
 router.post('/', (req, res) => {
   const { errors, isValid } = validateInput(req.body);
 
-  if(!isValid) {
+  if(isValid) {
+    res.json({ success: true });
+  } else {
     res.status(400).json(errors);
   }
 });
